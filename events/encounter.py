f2b48fc7f638c109524119d0fddcec8dd85000a4
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-26 21:37:18 +0200

    correct models

---
+++ events/encounter.py
@@ -0,0 +1,18 @@
+import pygame
+
+def encounter(fight, event, list_of_events):
+    if fight.enemy.is_alive():
+            fight.blit_objects()
+    for event in list_of_events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                fight.player.attack(fight.enemy)
+    
+    if fight.player.is_alive() and fight.enemy.is_alive():
+        fight.enemy.move(fight.player)
+    elif fight.enemy.is_alive() == False:
+        fight.grant_prize()
+        fight.player.is_fighting = False
+        return fight.player.is_fighting
+    if pygame.Rect.colliderect(fight.player.rect, fight.enemy.rect):
+        fight.enemy.attack(fight.player)
